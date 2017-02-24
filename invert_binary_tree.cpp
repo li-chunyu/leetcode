1eb4e1451d1720b9fbd34cd487d4320eeac4dcbb
lichunyu <li.chunyu@eisoo.com>
2017-02-24 16:01:49 +0800

    bfs

--- invert_binary_tree.cpp
+++ invert_binary_tree.cpp
@@ -28,3 +28,48 @@ public:
   			
     }
 };
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+ 
+ /*
+  BFS solution
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        
+        if (NULL == root) {
+            return root;
+        }
+
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        while (!q.empty()) {
+            TreeNode* temp_node = q.front();
+            q.pop();
+            
+            if (NULL != temp_node->left) {
+                q.push(temp_node->left);
+            }
+            
+            if (NULL != temp_node->right) {
+                q.push(temp_node->right);
+            }
+            
+            TreeNode* temp = temp_node->left;       /* exchange left child tree and right tree */
+            temp_node->left = temp_node->right;
+            temp_node->right = temp;
+        }
+        
+  		return root;	
+    }
+};

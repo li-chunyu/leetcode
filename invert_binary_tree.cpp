7a91114051100e1da70b6c54fc261fde98558310
lichunyu <li.chunyu@eisoo.com>
2017-02-24 15:42:23 +0800

    day day up

---
+++ invert_binary_tree.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+
+  		if (root == NULL || (root->left==NULL && root->right==NULL) ) {
+  			return root;
+  		}
+
+  		// Exchange child trees.
+  		TreeNode* temp = NULL;
+  		temp = root->left;
+  		root->left = root->right;
+  		root->right = temp;
+
+  		invertTree(root->right);
+  		invertTree(root->left);
+
+  		return root;
+  			
+    }
+};

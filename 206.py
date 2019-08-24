793599ea740781abb54a01130e63a6a89113019f
lichunyu <mnz.lichunyu@gmail.com>
2019-08-24 13:17:33 +0800

    prob 206 recur version

--- 206.py
+++ 206.py
@@ -17,3 +17,19 @@ class Solution:
             r = tmp
         head.next = None
         return l
+
+# Recursion version 
+class Solution:
+    def RecReverseList(self, head: ListNode) -> ListNode:
+        if head == None or head.next == None:
+            return head
+        h, t = self.reverse(head)
+        t.next = None
+        return h
+         
+    def reverse(self, head):
+        if head.next == None:
+            return head, head #(head, tail)
+        h, t = self.reverse(head.next)
+        t.next = head
+        return h, head

5d68e00b41c6021909d4a0272b9a9eb49c7ec873
lichunyu <mnz.lichunyu@gmail.com>
2019-12-12 10:20:16 +0800

    844 better solution, time O(n), space O(1)844

--- 844.cc
+++ 844.cc
@@ -1,3 +1,39 @@
+// time O(n)
+// space O(1)
+class Solution {
+public:
+    bool backspaceCompare(string S, string T) {
+      int idx1 = S.size() - 1, idx2 = T.size() - 1;
+      while (idx1 >= 0 || idx2 >= 0) {
+        auto p1 = get_char(S, idx1);
+        auto p2 = get_char(T, idx2);
+        if (p1.first != p2.first)
+          return false;
+        idx1 = p1.second;
+        idx2 = p2.second;
+      }
+      return true;
+    }
+    
+    pair<char, int> get_char(string s, int idx) {
+      char c = '\0';
+      int  skip = 0;
+      while (idx >= 0 && c == '\0') {
+        if (s[idx] == '#') {
+          skip++;
+        } else if (skip != 0) {
+          skip--;
+        } else {
+          c = s[idx];
+        }
+        idx--;
+      }
+      return make_pair(c, idx);
+    }
+};
+
+
+// O(n) O(n)
 class Solution {
 public:
     bool backspaceCompare(string S, string T) {
